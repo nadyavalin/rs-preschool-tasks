c0a9d5996ba1faa63c778edbdaa17b985fc275b0
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-08-16 23:02:14 +0300

    fix: do better code in About block for future slider

--- library/js/script.js
+++ library/js/script.js
@@ -1,21 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const header = document.querySelector(".header");
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector(".header");
 
-    document.querySelector(".header__burger-btn").addEventListener("click", function() {
-        setTimeout(() => {
-          header.classList.toggle("open");
-        }, 0)
-    })
-
-    document.addEventListener('keydown', (e) => {
-        if (e.key === "Escape") {
-            header.classList.remove("open");
-        }
+  document
+    .querySelector(".header__burger-btn")
+    .addEventListener("click", function () {
+      setTimeout(() => {
+        header.classList.toggle("open");
+      }, 0);
     });
 
-    document.body.addEventListener('click', event => {
-        if (!event.target.classList.contains("menu") && !event.target.closest(".header__burger-btn") && header.classList.contains("open")) {        
-            header.classList.remove("open");
-        }
-    });
-})
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      header.classList.remove("open");
+    }
+  });
+
+  document.body.addEventListener("click", (event) => {
+    if (
+      !event.target.classList.contains("menu") &&
+      !event.target.closest(".header__burger-btn") &&
+      header.classList.contains("open")
+    ) {
+      header.classList.remove("open");
+    }
+  });
+});
+
+// Slider in About block
