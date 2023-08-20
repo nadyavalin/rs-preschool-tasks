ebdf910710c28e595723a5e4e5bba225356f253b
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-08-20 23:00:58 +0300

    feat: add sticky to season-block and made pop-up register

--- library/js/script.js
+++ library/js/script.js
@@ -16,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
+  /*
   document.body.addEventListener("click", (event) => {
     if (
       !event.target.classList.contains("menu") &&
@@ -25,13 +26,14 @@ document.addEventListener("DOMContentLoaded", function () {
       header.classList.remove("open");
     }
   });
+  */
 });
 
+// Next block
 // Slider in About block
 let offset = 0;
 let activeImageIndex = 0;
 let imageWidth = window.innerWidth > 768 ? 475 : 450;
-
 const images = document.querySelectorAll(".image");
 const imagesLine = document.querySelector(".images-line");
 const paginationDots = document.querySelectorAll(".pagination-dot");
@@ -93,5 +95,166 @@ window.addEventListener("resize", function () {
     imageWidth = 475;
   } else {
     imageWidth = 450;
+  }
+});
+
+// Next block
+// Slider in Favorites block
+const fadeIn = (element, timeout, display) => {
+  element.style.opacity = 0;
+  element.style.display = display || "block";
+  element.style.transition = `opacity ${timeout}ms`;
+  setTimeout(() => {
+    element.style.opacity = 1;
+  }, 700);
+};
+
+const fadeOut = (element, timeout) => {
+  element.style.opacity = 1;
+  element.style.transition = `opacity ${timeout}ms`;
+  element.style.opacity = 0;
+  setTimeout(() => {
+    element.style.display = "none";
+  }, timeout);
+};
+
+const btnWinter = document.querySelector(".season-block__item_winter");
+const btnSpring = document.querySelector(".season-block__item_spring");
+const btnSummer = document.querySelector(".season-block__item_summer");
+const btnAutumn = document.querySelector(".season-block__item_autumn");
+
+const bookCardsWinter = document.querySelector(".book-cards__winter");
+const bookCardsSpring = document.querySelector(".book-cards__spring");
+const bookCardsSummer = document.querySelector(".book-cards__summer");
+const bookCardsAutumn = document.querySelector(".book-cards__autumn");
+
+/*
+btnWinter.addEventListener("hover", (e) => {
+  if (radio === checked) {
+    btnWinter.classList.remove("season-block__item");
+  } else return;
+});
+*/
+
+let flagWinter = false;
+
+btnWinter.addEventListener("click", () => {
+  if (flagWinter) {
+    fadeOut(bookCardsSummer, 700);
+    fadeOut(bookCardsAutumn, 700);
+    fadeOut(bookCardsSpring, 700);
+    flagWinter = false;
+  } else {
+    fadeIn(bookCardsWinter, 700, "flex");
+    flagWinter = true;
+  }
+});
+
+let flagSpring = false;
+
+btnSpring.addEventListener("click", () => {
+  if (flagSpring) {
+    fadeOut(bookCardsWinter, 700);
+    fadeOut(bookCardsSummer, 700);
+    fadeOut(bookCardsAutumn, 700);
+    flagSpring = false;
+  } else {
+    fadeIn(bookCardsSpring, 700, "flex");
+    flagSpring = true;
+  }
+});
+
+let flagSummer = false;
+
+btnSummer.addEventListener("click", () => {
+  if (flagSummer) {
+    fadeOut(bookCardsSpring, 700);
+    fadeOut(bookCardsWinter, 700);
+    fadeOut(bookCardsAutumn, 700);
+    flagSummer = false;
+  } else {
+    fadeIn(bookCardsSummer, 700, "flex");
+    flagSummer = true;
+  }
+});
+
+let flagAutumn = false;
+
+btnAutumn.addEventListener("click", () => {
+  if (flagAutumn) {
+    fadeOut(bookCardsWinter, 700);
+    fadeOut(bookCardsSpring, 700);
+    fadeOut(bookCardsSummer, 700);
+    flagAutumn = false;
+  } else {
+    fadeIn(bookCardsAutumn, 700, "flex");
+    flagAutumn = true;
+  }
+});
+
+/*
+btnWinter.addEventListener("click", () => {
+  fadeOut(bookCardsSummer, 700);
+  fadeOut(bookCardsAutumn, 700);
+  fadeOut(bookCardsSpring, 700);
+  fadeIn(bookCardsWinter, 700, "flex");
+});
+
+btnSpring.addEventListener("click", () => {
+  fadeOut(bookCardsWinter, 700);
+  fadeOut(bookCardsSummer, 700);
+  fadeOut(bookCardsAutumn, 700);
+  fadeIn(bookCardsSpring, 700, "flex");
+});
+
+btnSummer.addEventListener("click", () => {
+  fadeOut(bookCardsSpring, 700);
+  fadeOut(bookCardsWinter, 700);
+  fadeOut(bookCardsAutumn, 700);
+  fadeIn(bookCardsSummer, 700, "flex");
+});
+
+btnAutumn.addEventListener("click", () => {
+  fadeOut(bookCardsWinter, 700);
+  fadeOut(bookCardsSpring, 700);
+  fadeOut(bookCardsSummer, 700);
+  fadeIn(bookCardsAutumn, 700, "flex");
+});
+*/
+
+// Profile no auth
+const profile = document.querySelector(".profile__icon");
+const noAuth = document.querySelector(".profile__no-auth_active");
+const withAuth = document.querySelector(".profile__with-auth_active");
+
+profile.addEventListener("click", function () {
+  setTimeout(() => {
+    noAuth.classList.toggle("open");
+  }, 0);
+});
+
+/* // не работает
+document.body.addEventListener("click", (event) => {
+  if (
+    !event.target.classList.contains("profile") &&
+    !event.target.closest(".profile__no-auth_active") &&
+    profile.classList.contains("open")
+  ) {
+    profile.classList.remove("open");
+  }
+});
+*/
+
+// Pop Up Register
+const btnRegister = document.querySelector(".btn__register");
+const popUpRegister = document.querySelector('.pop-up__register');
+
+btnRegister.addEventListener('click', () => {
+  popUpRegister.classList.toggle('hidden')
+});
+
+popUpRegister.addEventListener('click', (event) => {
+  if (event.target.classList.contains('pop-up__register')) {
+    popUpRegister.classList.toggle('hidden');
   }
 });
