9f9480870d1212d98a591268a53b39c67901fb6c
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-09-20 02:31:59 +0300

    feat: add some styles for the view of the player (Wed, Sep 20, 2023 02:31:58 AM)

--- audio-player/script.js
+++ audio-player/script.js
@@ -1,5 +1,3 @@
-let isPlay = false;
-
 const songs = ["./assets/audio/Bad Omens - The Death of Peace of Mind.mp3",
                 "./assets/audio/Ethan Bortnick - Engravings.mp3",
                 "./assets/audio/Hensonn - Sahara.mp3"];
@@ -7,15 +5,21 @@ const songs = ["./assets/audio/Bad Omens - The Death of Peace of Mind.mp3",
 const backgrounds = ["./assets/img/bad-omens-big.jpg",
                     "./assets/img/engravings-big.jpg",
                     "./assets/img/sahara-big"];
-
+                    
 const images = ["./assets/img/bad-omens-small.jpg",
                     "./assets/img/engravings-small.jpg",
                     "./assets/img/sahara-small"];
-    
+
 const playBtn = document.querySelector(".btn__play-pause");
 const prevBtn = document.querySelector(".btn__prev");
 const nextBtn = document.querySelector(".btn__next");
+
+const songName = document.querySelector('.song-name');
+const singer = document.querySelector('.singer');
+
 const audio = new Audio();
+
+let isPlay = false;
 
 function playAudio() {
   audio.src = "./assets/audio/Bad Omens - The Death of Peace of Mind.mp3";
@@ -33,44 +37,49 @@ function playAudio() {
   }
 }
 
-const backgroundElement = document.querySelector("body");
-const imageElement = document.querySelector(".player");
-
-function changeBackgroundAndImage() {
-    backgroundElement.style.backgroundImage = url('${backgrounds[index]}');
-    imageElement.src = images[index];
-  }
-
-  
 let index = 0;
+let currentIndex = 0;
+
+function changeBackgroundNext() {
+  document.body.style.backgroundImage = url(`${backgrounds[currentIndex % backgrounds.length]}`);
+  currentIndex++;
+}
+
+function changeBackgroundPrev() {
+  document.body.style.backgroundImage = url(`${backgrounds[currentIndex % backgrounds.length]}`);
+  currentIndex--;
+}
 
 function prevSong() {
   audio.currentTime = 0;
   if (index === 0) {
     audio.src = songs[songs.length - 1];
-    index = songs[length - 1];
+    index = songs.length - 1;
   } else {
     audio.src = songs[--index];
+    currentIndex--;
   }
 
   if (isPlay) {
     audio.play();
   }
+  changeBackgroundPrev();
 }
 
 function nextSong() {
   audio.currentTime = 0;
-
   if (index === songs.length - 1) {
     audio.src = songs[0];
     index = 0;
   } else {
     audio.src = songs[++index];
+    currentIndex++;
   }
 
   if (isPlay) {
     audio.play();
   }
+  changeBackgroundNext();
 }
 
 playBtn.addEventListener("click", playAudio);
