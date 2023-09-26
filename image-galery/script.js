5b09c8f69729c104750bc59b3c64ca6c71589363
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-09-26 21:40:57 +0300

    feat: add the ability to request photos by clicking search or enter (Tue, Sep 26, 2023 09:40:57 PM)

--- image-galery/script.js
+++ image-galery/script.js
@@ -1,60 +1,47 @@
 const search = document.querySelector(".search-icon");
 const gallery = document.querySelector(".gallery");
 const errorMessage = document.querySelector(".error-msg");
-// const inputValue = document.querySelector(".number-input").value;
-// const url = `https://api.unsplash.com/photos?per_page=${inputValue}&page=1&client_id=Zz_TyElPfeBZoBIZmFlizrKQ9pzbIvRnBQSiz9WXdgU`;
 
 async function getImages() {
-    const inputValue = document.querySelector(".number-input").value;
-    if(inputValue > 30  || inputValue < 1) {
-        errorMessage.style.display = "block";
-        errorMessage.innerText = "Number should be between 1 and 30";
-        return;
-    }
+  const numberValue = document.querySelector(".number-input").value;
+  const searchValue = document.querySelector(".search-input").value;
+  const urlRandom = `https://api.unsplash.com/photos/random?count=${numberValue}&client_id=Zz_TyElPfeBZoBIZmFlizrKQ9pzbIvRnBQSiz9WXdgU`;
+  if (numberValue > 30 || numberValue < 1) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = "Number should be between 1 and 30";
+    return;
+  }
 
-    let imgs = "";
+  let url = "";
+  if (searchValue.trim() === "") {
+    url = urlRandom;
+  } else {
+    url = `https://api.unsplash.com/search/collections?per_page=${numberValue}&page=1&query=${searchValue}&client_id=Zz_TyElPfeBZoBIZmFlizrKQ9pzbIvRnBQSiz9WXdgU`;
+  }
 
-    try {
-        const loading = `<img src="./assets/svg/eclipse.svg">`;
-        gallery.innerHTML = loading;
-        await fetch(`https://api.unsplash.com/photos?per_page=${inputValue}&page=${Math.round(Math.random() * 1000)}&client_id=Zz_TyElPfeBZoBIZmFlizrKQ9pzbIvRnBQSiz9WXdgU`).then((res) =>
-        res.json().then((data) => {
-            if (data) {
-                data.forEach((img) => {
-                    imgs += `
-                    <img src="${img.urls.small} alt="image"/>`;
-                    /// gallery.style.display = "block";
-                    gallery.innerHTML = imgs;
-                    errorMessage.style.display = "none";
-                })
-            }
-            })
-        );
-    } catch (error) {
-        errorMessage.style.display = "block";
-        errorMessage.innerText = "Error, try again later";
-    }
-
+  let imgs = "";
+  try {
+    const loading = `<img src="./assets/svg/eclipse.svg">`;
+    gallery.innerHTML = loading;
+    const response = await fetch(url);
+    const data = await response.json();
+        if (data.results) {
+          data.results.forEach((img) => {
+            imgs += `<img src="${img.cover_photo.urls.small}" alt="image">`;
+          });
+          gallery.innerHTML = imgs;
+          errorMessage.style.display = "none";
+        }
+      } catch (error) {
+    errorMessage.style.display = "block";
+    errorMessage.innerText = "Error, try again later";
+  }
 }
-
-/*
-async function getImages() {
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-        images.src = data.file;
-    } catch (error) {
-        console.log(error);
-    }
-}
-getImages();
-
-search.addEventListener("click", () => {
-    const isLoaded = images.complete;
-    if (isLoaded) {
-    getImages();
-    }
-});
-*/
 
 search.addEventListener("click", getImages);
+document.querySelector(".search-input").addEventListener("keypress", e => {
+    if (e.key === "Enter") {
+        getImages();
+    }
+});
+window.addEventListener("DomContentLoaded", getImages);
