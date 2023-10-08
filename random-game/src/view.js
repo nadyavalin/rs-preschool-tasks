3bba945c7f3f98580955f515aa1cae47a69622c3
nadyavalin <nadejda.sergeevna.tkachuk@gmail.com>
2023-10-08 18:53:11 +0300

    docs: add file index.js with description and add some visual improvements into other files (Sun, Oct 08, 2023 06:53:11 PM)

--- random-game/src/view.js
+++ random-game/src/view.js
@@ -67,7 +67,7 @@ export default class View {
     this.context.textAlign = "center";
     this.context.textBaseLine = "middle";
     this.context.fillText(
-      "Press SPACE to Resume",
+      "Press SPACE or ENTER to Resume",
       this.width / 2,
       this.heigth / 2
     );
@@ -82,7 +82,7 @@ export default class View {
     this.context.fillText("GAME OVER", this.width / 2, this.heigth / 2 - 48);
     this.context.fillText(`Score: ${score}`, this.width / 2, this.heigth / 2);
     this.context.fillText(
-      `Press Enter to Restart`,
+      `Press ENTER to Restart`,
       this.width / 2,
       this.heigth / 2 + 48
     );
